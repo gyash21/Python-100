7db6462548f2c32ced4931ceea9e5e93f40600c6
Yash Gupta <gyash21@gmail.com>
2024-10-03 21:57:51 +0530

    added first project with basics

---
+++ Day01.py
@@ -0,0 +1,16 @@
+#print("Hello " + input("What is your name?") + "!")
+
+#number of char in a string
+# name = "Yash"
+# print(len(input("What is your name?")))
+
+# username = input("What is your name?")
+# length = len(username)
+# print("The name is " + username +" and the length is " + str(length))
+
+
+username = input("Hello there!, What is your name?\n")
+cityname = input("Which city do you live in?\n")
+petname = input("What is the name of your pet?\n")
+
+print("Your unique band name is " + cityname + " "+ petname)
