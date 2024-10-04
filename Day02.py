6c051fc624ff6963acf0201ae765a955c4a87865
Yash Gupta <gyash21@gmail.com>
2024-10-05 00:46:08 +0530

    basic data types

--- Day02.py
+++ Day02.py
@@ -1,6 +1,12 @@
 #type check
 
-print(type("Yash"))
-print(type(3.14))
-print(type(9))
-print(type(True))
+# print(type("Yash"))
+# print(type(3.14))
+# print(type(9))
+# print(type(True))
+
+
+name = input("Enter your name: ")
+length = len(name)
+
+print("Number of letter in your name is: " + str(length))
